805fc8cc02dfce301f9b8e244f903055619b79c6
Ritchie from Pokemon <aklnthndr@gmail.com>
2021-08-30 16:15:47 -0400

    I really dislike cors

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ mongoose.connect('mongodb://localhost/agent-washington')
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: '*.lbpunion.com'
+}))
 app.use(express.json());
 
 app.use('/api/v1', indexRouter);
