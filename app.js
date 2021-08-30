e3c66eed797a42253039b5e2f56e75d8fec79c8e
Nya0 <59615141+Nya0@users.noreply.github.com>
2021-08-30 14:12:05 -0700

    wok

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(express.json());
 
 app.use('/api/v1', indexRouter);
 
