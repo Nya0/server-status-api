a845197e9d13fa15ac818289c14b605b8bec2af6
Nya0 <59615141+Nya0@users.noreply.github.com>
2021-08-30 14:03:44 -0700

    d

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ mongoose.connect('mongodb://localhost/agent-washington')
 
 const app = express();
 
-app.use(cors({
-  origin: '*.lbpunion.com'
-}))
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 
 app.use('/api/v1', indexRouter);
